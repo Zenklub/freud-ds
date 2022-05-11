e98c8ecc5c4ba03379edac3ccf0f01428b6f795b
Douglas Laiber <douglaslbosco@gmail.com>
2022-05-11 15:58:52 -0300

    feat(tooltip): add tooltip component

---
+++ apps/core-web/src/app/components/tooltip/tooltip.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FreudTooltipComponent } from './tooltip.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [FreudTooltipComponent],
+  exports: [FreudTooltipComponent]
+})
+export class FreudTooltipModule { }
