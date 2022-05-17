105b4b32b55d84b8c8d17219ae5e8d971381a7af
Gabriel Vieira <gabrielvieira@Gabriels-MacBook-Pro.local>
2022-05-17 13:46:27 -0300

    feat: add new component tag in core-web

--- libs/core-web/src/lib/components/components.module.ts
+++ libs/core-web/src/lib/components/components.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { FreudAvatarModule } from './avatar';
 import { FreudButtonModule } from './button';
 import { FreudTooltipModule } from './tooltip';
+import { FreudTagModule } from './tag';
 
 @NgModule({
   imports: [
     FreudAvatarModule,
     FreudButtonModule,
     FreudTooltipModule,
+    FreudTagModule,
   ],
   exports: [
     FreudAvatarModule,
     FreudButtonModule,
     FreudTooltipModule,
+    FreudTagModule,
   ]
 })
 export class ComponentsModule { }
