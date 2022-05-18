f1a235c61db2238a243d08453b3393cff41e5c5f
Gabriel Vieira Teodoro <gabrielvt14@hotmail.com>
2022-05-18 17:21:47 -0300

    Revert "Feature/tesp 744"

--- libs/core-web/src/lib/components/components.module.ts
+++ libs/core-web/src/lib/components/components.module.ts
@@ -2,20 +2,17 @@ import { NgModule } from '@angular/core';
 import { FreudAvatarModule } from './avatar';
 import { FreudButtonModule } from './button';
 import { FreudTooltipModule } from './tooltip';
-import { FreudTagModule } from './tag';
 
 @NgModule({
   imports: [
     FreudAvatarModule,
     FreudButtonModule,
     FreudTooltipModule,
-    FreudTagModule,
   ],
   exports: [
     FreudAvatarModule,
     FreudButtonModule,
     FreudTooltipModule,
-    FreudTagModule,
   ]
 })
 export class ComponentsModule { }
