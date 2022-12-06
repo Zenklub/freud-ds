ffea7bbc0636e253fa8246ef7f3c4450af6c31d4
Bruno Chikuji <bruno-chikuji@hotmail.com>
2022-12-06 01:29:42 -0300

    Rating

--- packages/web/projects/web-components/src/components/rating/rating.component.ts
+++ packages/web/projects/web-components/src/components/rating/rating.component.ts
@@ -6,14 +6,11 @@ import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angu
     <p-rating
       [(ngModel)]="value"
       [stars]="stars"
-      [cancel]="cancel"
       [disabled]="disabled"
-      [readonly]="readonly"
       [iconOnClass]="iconOnClass"
       [iconOffClass]="iconOffClass"
-      [iconCancelClass]="iconCancelClass"
-      (onRate)="onRateChange($event)"
-      (onCancel)="onCancel.emit()">
+      [cancel]="false"
+      (onRate)="onRateChange($event)">
     </p-rating>
   `,
   styleUrls: ['./rating.component.scss'],
@@ -27,18 +24,14 @@ export class FreudRatingComponent {
 
   @Input() value: number = 0;
   @Input() stars: number = 5;
-  @Input() cancel: boolean = false;
   @Input() disabled: boolean = false;
-  @Input() readonly: boolean = false;
-  @Input() iconOnClass: string = 'freud-icon freud-icon-star';
+  @Input() iconOnClass: string = 'freud-icon freud-icon-star-fill';
   @Input() iconOffClass: string = 'freud-icon freud-icon-star';
-  @Input() iconCancelClass: string = 'freud-icon freud-icon-star';
   @Input() bgColor: boolean = false;
   @Output() onRate: EventEmitter<any> = new EventEmitter<any>();
   @Output() onCancel: EventEmitter<any> = new EventEmitter<any>();
 
   onRateChange(event: any) {
-    console.log(event);
     this.onRate.emit(event);
   }
 
--- packages/web/stories/rating/Rating.stories.ts
+++ packages/web/stories/rating/Rating.stories.ts
@@ -9,43 +9,30 @@ const Template: Story<FreudRatingComponent> = (
     <freud-rating
       [value]="value"
       [stars]="stars"
-      [cancel]="cancel"
       [disabled]="disabled"
-      [readonly]="readonly"
-      [iconOnClass]="iconOnClass"
-      [iconOffClass]="iconOffClass"
-      [iconCancelClass]="iconCancelClass"
       [bgColor]="bgColor">
     </freud-rating>
   `,
 });
 
 export const Default = Template.bind({});
+Default.args = {
+  value: 2
+}
 export const Disabled = Template.bind({});
 Disabled.args = {
+  value: 2,
   disabled: true
 };
-export const Readonly = Template.bind({});
-Readonly.args = {
-  value: 50,
-  readonly: true
-};
-
 export const BgColor = Template.bind({});
 BgColor.args = {
-  value: 80,
+  value: 2,
   bgColor: true,
 };
 
 export const BgColorDisabled = Template.bind({});
 BgColorDisabled.args = {
-  value: 80,
+  value: 2,
   bgColor: true,
   disabled: true
-};
-
-export const CancelButton = Template.bind({});
-CancelButton.args = {
-  value: 80,
-  cancel: true
 };
