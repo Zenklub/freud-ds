d802fb8f857040bd77df3b35c88171824102c02f
Bruno Chikuji <bruno-chikuji@hotmail.com>
2022-12-17 17:02:58 -0300

    feat: input group

--- packages/web/stories/forms/input-group/InputGroup.stories.ts
+++ packages/web/stories/forms/input-group/InputGroup.stories.ts
@@ -29,7 +29,9 @@ const TemplateIconAndRadio: Story<FreudInputTextComponent> = (args: FreudInputTe
     <div class="freud-input-group">
       <span class="freud-input-group-addon"><i class="freud-icon freud-icon-user"></i></span>
       <freud-input-text placeholder="Username"></freud-input-text>
-<!--      <span class="p-inputgroup-addon"></span>-->
+      <span class="freud-input-group-addon">
+        <freud-radio-button radioValue="teste" name="item"></freud-radio-button>
+      </span>
     </div>
   `,
 })
@@ -39,7 +41,9 @@ const TemplateRadioAndButton: Story<FreudInputTextComponent> = (args: FreudInput
   props: { ...args },
   template: `
     <div class="freud-input-group">
-        <!--      Aduardando Radio Button-->
+      <span class="freud-input-group-addon">
+        <freud-radio-button radioValue="teste" name="item2"></freud-radio-button>
+      </span>
       <freud-input-text placeholder="Username"></freud-input-text>
       <freud-button>Button</freud-button>
     </div>
@@ -49,5 +53,19 @@ export const RadioAndButton = TemplateRadioAndButton.bind({});
 
 export const BGColor = TemplateRadioAndButton.bind({});
 
+const TemplateCheckboxAndText: Story<FreudInputTextComponent> = (args: FreudInputTextComponent) => ({
+  props: { ...args },
+  template: `
+    <div class="freud-input-group">
+      <span class="freud-input-group-addon">
+        <freud-checkbox></freud-checkbox>
+      </span>
+      <freud-input-text placeholder="Username"></freud-input-text>
+      <span class="freud-input-group-addon">www</span>
+    </div>
+  `,
+})
+
+export const CheckboxAndText = TemplateCheckboxAndText.bind({});
 
 
