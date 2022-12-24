16e5773bb055bc585c59ec175b3936a695fc4da6
Bruno Chikuji <bruno-chikuji@hotmail.com>
2022-12-24 06:07:30 -0300

    package-lock

---
+++ packages/web/projects/web-components/src/components/others/timeline/timeline.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'freud-scroll-top',
+  template: `
+    <p-timeline [threshold]="threshold" [target]="target" [behavior]="behavior"></p-timeline>
+  `,
+  styleUrls: ['./timeline.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'freud-scroll-top',
+  }
+})
+export class FreudScrollTopComponent {
+
+  @Input() threshold: number = 400;
+  @Input() target: 'window' | 'parent' = 'window';
+  @Input() behavior: 'smooth' | 'auto' = 'smooth';
+  @Input() icon: string = 'freud-icon freud-icon-chevron-up';
+
+}
