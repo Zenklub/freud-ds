a614e2a8e0634e16161651ca3751b6780fde9118
Kayran dos Santos <kayran.santos@zenklub.com>
2023-02-02 12:50:49 -0300

    feat: adding loader in ds v2

---
+++ packages/web/stories/feedback/loader/Loader.stories.ts
@@ -0,0 +1,47 @@
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+import {FreudLoaderComponent, FreudLoaderModule} from "../../../projects/web-components/src/components/feedback/loader";
+
+const Template: Story<FreudLoaderComponent> = (
+  args: FreudLoaderComponent
+) => ({
+  props: { ...args },
+  template: `
+    <freud-loader
+      [size]="size"
+      [bgColor]="bgColor">
+    </freud-loader>
+  `,
+});
+
+export const Default = Template.bind({});
+Default.args = {};
+
+const TemplateBG: Story<FreudLoaderComponent> = (
+  args: FreudLoaderComponent
+) => ({
+  props: {...args},
+  template:`
+    <freud-loader style="margin-right: 20px;"
+      [bgColor]="true">
+    </freud-loader>
+  `,
+});
+export const BGColor = TemplateBG.bind({});
+
+const TemplateSize: Story<FreudLoaderComponent> = (
+  args: FreudLoaderComponent
+) => ({
+  props: { ...args },
+  template: `
+    <div style="display: inline-flex;align-items: center;">
+      <freud-loader style="margin-right: 20px;"
+        [size]="'sm'">
+      </freud-loader>
+
+      <freud-loader
+        [size]="'lg'">
+      </freud-loader>
+    </div>
+  `,
+});
+export const Size = TemplateSize.bind({});
