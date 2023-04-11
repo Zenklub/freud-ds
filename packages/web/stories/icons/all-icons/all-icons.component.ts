f8250ed36d46b3c87e3599ade54d1b1ff51b4bba
Kayran dos Santos <kayran.santos@zenklub.com>
2023-04-11 18:32:11 -0300

    feat(plat-4): adding icon storybook and returning with size properties

--- packages/web/stories/icons/all-icons/all-icons.component.ts
+++ packages/web/stories/icons/all-icons/all-icons.component.ts
@@ -6,18 +6,24 @@ import iconNamesJson from './icon-names.json';
 @Component({
   selector: 'all-icons',
   templateUrl: 'all-icons.component.html',
+  styleUrls: ['all-icons.component.scss'],
 })
 export class AllIconsComponent implements OnInit {
   iconNames:string[] = [];
+  fullIconNames:string[] = [];
+
   baseMessage: string = 'Copied to clipboard';
   message: string = this.baseMessage;
+
+  searchText: string = '';
 
   constructor(private clipboardService: ClipboardService) {}
   // constructor(private clipboardService: ClipboardService, private messageService: FreudMessageService) {}
 
   ngOnInit(): void {
     console.log(iconNamesJson);
-    this.iconNames = iconNamesJson;
+    this.fullIconNames = iconNamesJson;
+    this.filterIcons();
   }
 
   clipboard(iconName: string) {
@@ -26,10 +32,21 @@ export class AllIconsComponent implements OnInit {
     this.toast(iconName);
   }
 
+  filterIcons($event?: any) {
+    if($event)  {
+      this.searchText = $event.srcElement.value.toLowerCase();
+    }
+
+    if (this.searchText) {
+      this.iconNames = this.fullIconNames.filter((iconName) => iconName.includes(this.searchText));
+    } else {
+      this.iconNames = iconNamesJson;
+    }
+  }
+
+
   toast(iconName: string) {
     this.message = `"${iconName}" ${this.baseMessage}!`
     console.log(this.message);
-    // const message = {severity:'success', summary:'Service Message', detail:'Via MessageService'};
-    // this.messageService.add(message);
   }
 }
--- packages/web/stories/icons/all-icons/all-icons.module.ts
+++ packages/web/stories/icons/all-icons/all-icons.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FreudAlertMessagesModule  } from '@freud-ds/web-components';
+
 import { ClipboardModule } from 'ngx-clipboard';
 import { AllIconsComponent } from './all-icons.component';
+import { FreudInputTextModule } from '@freud-ds/web-components';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [AllIconsComponent],
-  imports: [BrowserModule, ClipboardModule, FreudAlertMessagesModule],
+  imports: [BrowserModule, ClipboardModule, FreudInputTextModule, FormsModule],
   exports: [AllIconsComponent],
 })
 export class AllIconsModule { }
