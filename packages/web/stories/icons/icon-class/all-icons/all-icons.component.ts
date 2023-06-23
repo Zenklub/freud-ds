8218aa157af657568addc87c76d6b71dd9b5889e
Kayran Santos <474481+kayran@users.noreply.github.com>
2023-06-23 14:10:29 -0300

    feat(PLAT-4): adding freud-icon lib listing in storybook (#77)
    * feat(plat-4): adding icon storybook and returning with size properties
    
    * feat(plat-4): adding icon storybook and returning with size properties
    
    * feat(plat-4): adding icon storybook and returning with size properties
    
    * feat(plat-4): adding icon storybook and returning with size properties
    
    * feat(plat-4): adding icon storybook and returning with size properties
    
    * feat(plat-4): adding icon storybook and returning with size properties
    
    * feat(plat-4): adding icon storybook and returning with size properties
    
    * Update packages/web/stories/icons/icon-class/all-icons/all-icons.component.ts
    
    * Update packages/web/stories/icons/icon-class/all-icons/all-icons.component.ts
    
    * Update packages/web/stories/icons/icon-class/all-icons/all-icons.component.ts
    
    ---------
    
    Co-authored-by: Kayran dos Santos <kayran.santos@zenklub.com>

---
+++ packages/web/stories/icons/icon-class/all-icons/all-icons.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
+
+import iconNamesJson from './icon-names.json';
+
+@Component({
+  selector: 'all-icons',
+  templateUrl: 'all-icons.component.html',
+  styleUrls: ['all-icons.component.scss'],
+})
+export class AllIconsComponent implements OnInit {
+  iconNames:string[] = [];
+  fullIconNames:string[] = [];
+
+  baseMessage: string = 'Copied to clipboard';
+  message: string = this.baseMessage;
+
+  searchText: string = '';
+
+  constructor(private clipboardService: ClipboardService) {}
+
+  ngOnInit(): void {
+    this.fullIconNames = iconNamesJson;
+    this.filterIcons();
+  }
+
+  clipboard(iconName: string) {
+    const freudClass = `freud-icon freud-icon-${iconName}`;
+    this.clipboardService.copyFromContent(freudClass);
+    this.toast(iconName);
+  }
+
+  filterIcons($event?: any) {
+    if($event)  {
+      this.searchText = $event.srcElement.value.toLowerCase();
+    }
+
+    if (this.searchText) {
+      this.iconNames = this.fullIconNames.filter((iconName) => iconName.includes(this.searchText));
+    } else {
+      this.iconNames = iconNamesJson;
+    }
+  }
+
+
+  toast(iconName: string) {
+    this.message = `"${iconName}" ${this.baseMessage}!`
+  }
+}
