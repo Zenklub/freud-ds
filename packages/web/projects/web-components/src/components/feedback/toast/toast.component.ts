628ca0b9faa4b2c7a626a39ca96c320904b33504
Ramon Ferreira <ramon.ferreira@zenklub.com>
2023-09-29 11:52:17 -0300

    chore: update develop branch (#97)
    * feat(TSAU-1590): clickable toast (#95)
    
    * feat(TSAU-1590): clickable toast
    
    * chore: update package-lock
    
    * fix: install with --legacy-peer-deps
    
    * fix: install with --legacy-peer-deps
    
    * fix: remove non related change
    
    * fix: non-null to optional input
    
    * fix: code style and no call
    
    * feat: add optional new tab open
    
    * fix: remove unused style
    
    * fix: remove example url
    
    * chore: update package-lock with node v16.20.2
    
    * chore: update package-lock with node v16.20.2
    
    * feat: add target options to open url
    
    * chore: update package-lock with node v16.20.2
    
    * chore(web-components): automatic version update
    
    ---------
    
    Co-authored-by: Zenklub Deploy Bot <bot@zenklub.com>

--- packages/web/projects/web-components/src/components/feedback/toast/toast.component.ts
+++ packages/web/projects/web-components/src/components/feedback/toast/toast.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, ViewEncapsulation } from "@angular/core";
 
 type positions = "top-right" | "top-left" | "bottom-left" | "bottom-right" | "top-center" | "bottom-center" | "center";
+type targetOptions = '_blank' | '_self' | '_top' | '_parent';
 
 @Component({
   selector: 'freud-toast',
   template: `
-    <p-toast [position]="position" [key]="key"></p-toast>
+    <p-toast [position]="position" [key]="key" [class.clickable]="url" (click)="url && openURL($event)"></p-toast>
   `,
   styleUrls: ['./toast.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -17,5 +18,14 @@ export class FreudToastComponent {
 
   @Input() position: positions = 'top-right';
   @Input() key!: string;
+  @Input() url?: string;
+  @Input() targetParam: targetOptions = '_blank';
 
+  openURL(event: any): void {
+    const isCloseBtn = event.srcElement.className.includes('close-icon');
+
+    if (!isCloseBtn && this.url) {
+      window.open(this.url, this.targetParam);
+    }
+  }
 }
