f1e56344215fc0e1d2661ccca4d91ed2a47f9e32
Gabrielle Fonseca <gabrielle.fonseca@zenklub.com>
2023-10-11 11:58:22 -0300

    feat: updating icon-class page

--- packages/web/stories/icons/icon-class/Icons.stories.ts
+++ packages/web/stories/icons/icon-class/Icons.stories.ts
@@ -1,6 +1,6 @@
-import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { Meta, moduleMetadata } from '@storybook/angular';
 
-import { ClipboardModule, ClipboardService } from 'ngx-clipboard';
+import { ClipboardModule } from 'ngx-clipboard';
 import { AllIconsComponent } from './all-icons/all-icons.component';
 
 
@@ -18,20 +18,34 @@ export const AllIcons = () => ({
   component: AllIconsComponent,
 });
 
-export const Verificated = () => {
+export const Verificated_i = () => {
   return {
     template: `
       <i class='freud-icon freud-icon-verificated' style='font-size: 24px'></i>
+     `
+  };
+};
+
+export const VerificatedSmall_i = () => {
+  return {
+    template: `
+      <i class='freud-icon freud-icon-verificated freud-icon-small' style='font-size: 16px'></i>
+     `
+  };
+};
+
+export const Verificated_span = () => {
+  return {
+    template: `
       <span class='freud-icon freud-icon-verificated' style='font-size: 24px'></span>
      `
   };
 };
 
-export const VerificatedSmall = () => {
+export const VerificatedSmall_span= () => {
   return {
     template: `
-      <i class='freud-icon freud-icon-verificated freud-icon-small' style='font-size: 14px'></i>
-      <span class='freud-icon freud-icon-verificated freud-icon-small' style='font-size: 14px'></span>
+      <span class='freud-icon freud-icon-verificated freud-icon-small' style='font-size: 16px'></span>
      `
   };
 };
