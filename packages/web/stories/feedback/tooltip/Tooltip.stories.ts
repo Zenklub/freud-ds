21f13f2e4a1ab73370b6af487814023e75b13e87
Gabrielle Fonseca <gabrielle.fonseca@zenklub.com>
2023-12-06 11:06:14 -0300

    feat: fixing doc and adding Disabled description

--- packages/web/stories/feedback/tooltip/Tooltip.stories.ts
+++ packages/web/stories/feedback/tooltip/Tooltip.stories.ts
@@ -14,31 +14,56 @@ const Template: Story<FreudTooltipComponent> = (
   `,
 });
 
-export const Top = Template.bind({});
-Top.args = {
-  tooltipLabel: 'Tooltip label top',
-  tooltipPosition: 'top'
+//Teste
+export const TooltipTest = Template.bind({});
+
+// Position
+export const Top = () => {
+  return {
+    template: `
+      <span freud-tooltip tooltipLabel="Tooltip label top" tooltipPosition="top" style="font-family: 'Source Sans Pro'">Passe o mouse aqui!</span>
+    `,
+  };
 };
-export const Bottom = Template.bind({});
-Bottom.args = {
-  tooltipLabel: 'Tooltip label bottom',
-  tooltipPosition: 'bottom'
+
+export const Bottom = () => {
+  return {
+    template: `
+      <span freud-tooltip tooltipLabel="Tooltip label bottom" tooltipPosition="bottom" style="font-family: 'Source Sans Pro'">Passe o mouse aqui!</span>
+    `,
+  };
 };
-export const Right = Template.bind({});
-Right.args = {
-  tooltipLabel: 'Tooltip label right',
-  tooltipPosition: 'right'
+
+export const Right = () => {
+  return {
+    template: `
+      <span freud-tooltip tooltipLabel="Tooltip label right" tooltipPosition="right" style="font-family: 'Source Sans Pro'">Passe o mouse aqui!</span>
+    `,
+  };
 };
-export const Left = Template.bind({});
-Left.args = {
-  tooltipLabel: 'Tooltip label left',
-  tooltipPosition: 'left'
+
+export const Left = () => {
+  return {
+    template: `
+      <span freud-tooltip tooltipLabel="Tooltip label left" tooltipPosition="left" style="font-family: 'Source Sans Pro'">Passe o mouse aqui!</span>
+    `,
+  };
 };
 
 // BgColor
-export const BGColor = Template.bind({});
-BGColor.args = {
-  tooltipLabel: 'Com bgColor',
-  tooltipPosition: 'bottom',
-  bgColor: true
+export const BGColor = () => {
+  return {
+    template: `
+      <span freud-tooltip tooltipLabel="Tooltip label com BGColor" tooltipPosition="bottom" bgColor="true" style="font-family: 'Source Sans Pro'; color: 'white'">Passe o mouse aqui!</span>
+    `,
+  };
+};
+
+// Disabled
+export const Disabled = () => {
+  return {
+    template: `
+      <span freud-tooltip tooltipLabel="" tooltipPosition="bottom" style="font-family: 'Source Sans Pro'">Passe o mouse aqui!</span>
+    `,
+  };
 };
