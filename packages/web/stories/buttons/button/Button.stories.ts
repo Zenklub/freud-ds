a1394db5ccec28a1569513f09afe26cb2a821896
Gabrielle Fonseca <gabrielle.fonseca@zenklub.com>
2023-12-13 16:23:25 -0300

    feat: adding fullWidth to component and updating documentation

--- packages/web/stories/buttons/button/Button.stories.ts
+++ packages/web/stories/buttons/button/Button.stories.ts
@@ -16,90 +16,97 @@ ButtonTest.args = {
 // Theme
 export const Primary = () => {
   return {
-    template: `<freud-button [label]="'Button primary'"></freud-button>`,
+    template: `<freud-button label="Button primary"></freud-button>`,
   };
 };
 
 export const Secondary = () => {
   return {
-    template: `<freud-button [color]="'secondary'" [label]="'Button secondary'"></freud-button>`,
+    template: `<freud-button color="secondary" label="Button secondary"></freud-button>`,
   };
 };
 
 export const Ghost = () => {
   return {
-    template: `<freud-button [color]="'ghost'" [label]="'Button ghost'"></freud-button>`,
+    template: `<freud-button color="ghost" label="Button ghost"></freud-button>`,
   };
 };
 
 // Background
 export const BGColorPrimary = () => {
   return {
-    template: `<freud-button [bgColor]="true" [label]="'ButtonBG primary'"></freud-button>`,
+    template: `<freud-button [bgColor]="true" label="ButtonBG primary"></freud-button>`,
   };
 };
 
 export const BGColorSecondary = () => {
   return {
-    template: `<freud-button [bgColor]="true" [color]="'secondary'" [label]="'ButtonBG secondary'"></freud-button>`,
+    template: `<freud-button [bgColor]="true" color="secondary" label="ButtonBG secondary"></freud-button>`,
   };
 };
 
 export const BGColorGhost = () => {
   return {
-    template: `<freud-button [bgColor]="true" [color]="'ghost'" [label]="'ButtonBG ghost'"></freud-button>`,
+    template: `<freud-button [bgColor]="true" color="ghost" label="ButtonBG ghost"></freud-button>`,
   };
 };
 
 // Disabled
 export const Disabled = () => {
   return {
-    template: `<freud-button [disabled]="true" [label]="'Button disabled'"></freud-button>`,
+    template: `<freud-button [disabled]="true" label="Button disabled"></freud-button>`,
   };
 };
 
 // Loading
 export const Loading = () => {
   return {
-    template: `<freud-button [loading]="true" [bgColor]="false" [label]="'Button loading'" [iconPos]="'right'"></freud-button>`,
+    template: `<freud-button [loading]="true" [bgColor]="false" label="Button loading" iconPos="right"></freud-button>`,
   };
 };
 
 // Sizes
 export const SizeLg = () => {
   return {
-    template: `<freud-button [bgColor]="bgColor" [size]="'lg'" [label]="'Button large'"></freud-button>`,
+    template: `<freud-button [bgColor]="bgColor" size="lg" label="Button large"></freud-button>`,
   };
 };
 
 export const SizeMd = () => {
   return {
-    template: `<freud-button [bgColor]="bgColor" [size]="'md'" [label]="'Button medium'"></freud-button>`,
+    template: `<freud-button [bgColor]="bgColor" size="md" label="Button medium"></freud-button>`,
   };
 };
 
 export const SizeSm = () => {
   return {
-    template: `<freud-button [bgColor]="bgColor" [size]="'sm'" [label]="'Button small'"></freud-button>`,
+    template: `<freud-button [bgColor]="bgColor" size="sm" label="Button small"></freud-button>`,
+  };
+};
+
+// Full Width
+export const fullWidth = () => {
+  return {
+    template: `<freud-button [bgColor]="bgColor" [fullWidth]="true" label="Button fullWidth"></freud-button>`,
   };
 };
 
 // With icon 
 export const IconOnly = () => {
   return {
-    template: `<freud-button [icon]="'freud-icon freud-icon-check'"></freud-button>`,
+    template: `<freud-button icon="freud-icon freud-icon-check"></freud-button>`,
   };
 };
 
 export const WithIconRight = () => {
   return {
-    template: `<freud-button [icon]="'freud-icon freud-icon-check'" [label]="'Button right'" [iconPos]="'right'"></freud-button>`,
+    template: `<freud-button icon="freud-icon freud-icon-check" label="Button right" iconPos="right"></freud-button>`,
   };
 };
 
 export const WithIconLeft = () => {
   return {
-    template: `<freud-button [icon]="'freud-icon freud-icon-check'" [label]="'Button left'"></freud-button>`,
+    template: `<freud-button icon="freud-icon freud-icon-check" label="Button left"></freud-button>`,
   };
 };
 
