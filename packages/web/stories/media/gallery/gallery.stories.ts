d689ad64f722eb5cd22e2fd73492ff57071b6871
Ligia Goncalves <ligia.goncalves@zenklub.com>
2024-06-13 16:22:42 -0300

    feat: allow gallery to accept video and image - examples doc

--- packages/web/stories/media/gallery/gallery.stories.ts
+++ packages/web/stories/media/gallery/gallery.stories.ts
@@ -1,69 +1,96 @@
-import { FreudGalleryComponent } from '@freud-ds/web-components';
+import { FreudGalleryComponent, GalleryPhoto } from '@freud-ds/web-components';
 import { Story, applicationConfig } from '@storybook/angular';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 
-const images = [
+const images: GalleryPhoto[] = [
   {
+    type: "image",
     previewImageSrc: 'assets/img/gallery/galleria1.jpg',
     thumbnailImageSrc: 'assets/img/gallery/galleria1.jpg',
-    alt: 'Description for Image 1',
-    title: 'Title 1',
+    alt: "Description for Image 1",
+    title: "Title 1"
   },
   {
-    previewImageSrc: 'assets/img/gallery/galleria2.jpg',
-    thumbnailImageSrc: 'assets/img/gallery/galleria2.jpg',
-    alt: 'Description for Image 2',
-    title: 'Title 2',
+    type: "image",
+    previewImageSrc: "assets/img/gallery/galleria2.jpg",
+    thumbnailImageSrc: "assets/img/gallery/galleria2.jpg",
+    alt: "Description for Image 2",
+    title: "Title 2"
   },
   {
-    previewImageSrc: 'assets/img/gallery/galleria3.jpg',
-    thumbnailImageSrc: 'assets/img/gallery/galleria3.jpg',
-    alt: 'Description for Image 3',
-    title: 'Title 3',
+    type: "image",
+    previewImageSrc: "assets/img/gallery/galleria3.jpg",
+    thumbnailImageSrc: "assets/img/gallery/galleria3.jpg",
+    alt: "Description for Image 3",
+    title: "Title 3"
   },
   {
-    previewImageSrc: 'assets/img/gallery/galleria4.jpg',
-    thumbnailImageSrc: 'assets/img/gallery/galleria4.jpg',
-    alt: 'Description for Image 4',
-    title: 'Title 4',
+    type: "image",
+    previewImageSrc: "assets/img/gallery/galleria4.jpg",
+    thumbnailImageSrc: "assets/img/gallery/galleria4.jpg",
+    alt: "Description for Image 4",
+    title: "Title 4"
   },
   {
-    previewImageSrc: 'assets/img/gallery/galleria5.jpg',
-    thumbnailImageSrc: 'assets/img/gallery/galleria5.jpg',
-    alt: 'Description for Image 5',
-    title: 'Title 5',
+    type: "image",
+    previewImageSrc: "assets/img/gallery/galleria5.jpg",
+    thumbnailImageSrc: "assets/img/gallery/galleria5.jpg",
+    alt: "Description for Image 5",
+    title: "Title 5"
   },
   {
-    previewImageSrc: 'assets/img/gallery/galleria6.jpg',
-    thumbnailImageSrc: 'assets/img/gallery/galleria6.jpg',
-    alt: 'Description for Image 6',
-    title: 'Title 6',
+    type: "image",
+    previewImageSrc: "assets/img/gallery/galleria6.jpg",
+    thumbnailImageSrc: "assets/img/gallery/galleria6.jpg",
+    alt: "Description for Image 6",
+    title: "Title 6"
   },
   {
-    previewImageSrc: 'assets/img/gallery/galleria7.jpg',
-    thumbnailImageSrc: 'assets/img/gallery/galleria7.jpg',
-    alt: 'Description for Image 7',
-    title: 'Title 7',
+    type: "image",
+    previewImageSrc: "assets/img/gallery/galleria7.jpg",
+    thumbnailImageSrc: "assets/img/gallery/galleria7.jpg",
+    alt: "Description for Image 7",
+    title: "Title 7"
   },
   {
-    previewImageSrc: 'assets/img/gallery/galleria8.jpg',
-    thumbnailImageSrc: 'assets/img/gallery/galleria8.jpg',
-    alt: 'Description for Image 8',
-    title: 'Title 8',
+    type: "image",
+    previewImageSrc: "assets/img/gallery/galleria8.jpg",
+    thumbnailImageSrc: "assets/img/gallery/galleria8.jpg",
+    alt: "Description for Image 8",
+    title: "Title 8"
   },
   {
-    previewImageSrc: 'assets/img/gallery/galleria9.jpg',
-    thumbnailImageSrc: 'assets/img/gallery/galleria9.jpg',
-    alt: 'Description for Image 9',
-    title: 'Title 9',
+    type: "image",
+    previewImageSrc: "assets/img/gallery/galleria9.jpg",
+    thumbnailImageSrc: "assets/img/gallery/galleria9.jpg",
+    alt: "Description for Image 9",
+    title: "Title 9"
   },
   {
-    previewImageSrc: 'assets/img/gallery/galleria10.jpg',
-    thumbnailImageSrc: 'assets/img/gallery/galleria10.jpg',
-    alt: 'Description for Image 10',
-    title: 'Title 10',
+    type: "image",
+    previewImageSrc: "assets/img/gallery/galleria10.jpg",
+    thumbnailImageSrc: "assets/img/gallery/galleria10.jpg",
+    alt: "Description for Image 10",
+    title: "Title 10"
+  }
+];
+
+const videos: GalleryPhoto[] = [
+  {
+    type: "video",
+    previewImageSrc: "https://www.youtube.com/embed/AurVL6n56wg",
+    thumbnailImageSrc: "https://www.youtube.com/embed/AurVL6n56wg",
+    alt: "Description for Image 1",
+    title: "Title 1"
+  },
+  {
+    type: "image",
+    previewImageSrc: "assets/img/gallery/galleria2.jpg",
+    thumbnailImageSrc: "assets/img/gallery/galleria2.jpg",
+    alt: "Description for Image 2",
+    title: "Title 2"
   },
 ];
 
@@ -83,6 +110,12 @@ const Template: Story<FreudGalleryComponent> = (
 export const Default = Template.bind({});
 Default.args = {
   images: images,
+  showThumbnails: true,
+};
+
+export const Videos = Template.bind({});
+Videos.args = {
+  images: videos,
   showThumbnails: true,
 };
 
