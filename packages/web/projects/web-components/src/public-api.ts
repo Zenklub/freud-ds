c666862a4075d12e90323be06f5132837372a94c
deividy hertz <deividy.hertz@zenklub.com>
2025-02-13 10:03:44 -0300

    fix: removed fullcalendar from freud-ds

--- packages/web/projects/web-components/src/public-api.ts
+++ packages/web/projects/web-components/src/public-api.ts
@@ -60,7 +60,6 @@ export * from './components/others/paginator';
 export * from './components/others/chip';
 export * from './components/others/captcha';
 export * from './components/others/table';
-export * from './components/others/full-calendar';
 export * from './components/others/accordion';
 
 // Structure
